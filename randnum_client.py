63e1492d0dc92669769bbef548e60aa4b8748ed6
Dominik Brugger <dominikbrugger@posteo.de>
2017-09-08 08:43:49 +0200

    Added valgrind results

--- randnum_client.py
+++ randnum_client.py
@@ -9,11 +9,15 @@ def run():
   stub = randnum_pb2_grpc.RandomNumbersStub(channel)
   gen = stub.GetNext(randnum_pb2.EmptyRequest())
   count = 0
+  step = 0
   for num in gen:
     if count % 10000 == 0:
       print(num.value)
       count = 0
+    if step == 20000:
+      break
     count += 1
+    step += 1
 
 if __name__ == '__main__':
   run()
